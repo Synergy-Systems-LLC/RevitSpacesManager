2ecc6fd1e5e44deeed47c9d4e751fbc0f323e670
AleksandrMaslov <amaslov8910@gmail.com>
2022-10-22 00:13:32 +0300

    Rename RevitAPIServices class to  RevitDocumentServices and define constructor

--- RevitSpacesManager/Revit/Services/RevitAPIServices.cs
+++ RevitSpacesManager/Revit/Services/RevitAPIServices.cs
@@ -2,8 +2,17 @@
 
 namespace RevitSpacesManager.Revit.Services
 {
-    internal static class RevitAPIServices
+    internal class RevitDocumentServices
     {
+        private Document _document;
+        private FilteredElementCollector _elementCollector;
+        private FilteredWorksetCollector _worksetCollector;
 
+        internal RevitDocumentServices(Document doc)
+        {
+            _document = doc;
+            _elementCollector = new FilteredElementCollector(doc);
+            _worksetCollector = new FilteredWorksetCollector(doc);
+        }
     }
 }
