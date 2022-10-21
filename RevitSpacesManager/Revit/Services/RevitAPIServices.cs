d7a71c24c612d6cead040d035f37dd7b1f50a4f2
AleksandrMaslov <amaslov8910@gmail.com>
2022-10-22 00:25:58 +0300

    Add DoesUserWorksetExist method to RevitDocumentServices class

--- RevitSpacesManager/Revit/Services/RevitAPIServices.cs
+++ RevitSpacesManager/Revit/Services/RevitAPIServices.cs
@@ -7,12 +7,27 @@ namespace RevitSpacesManager.Revit.Services
         private Document _document;
         private FilteredElementCollector _elementCollector;
         private FilteredWorksetCollector _worksetCollector;
+        private FilteredWorksetCollector _userWorksetCollector;
+
 
         internal RevitDocumentServices(Document doc)
         {
             _document = doc;
-            _elementCollector = new FilteredElementCollector(doc);
-            _worksetCollector = new FilteredWorksetCollector(doc);
+            _elementCollector = new FilteredElementCollector(_document);
+            _worksetCollector = new FilteredWorksetCollector(_document);
+            _userWorksetCollector = _worksetCollector.OfKind(WorksetKind.UserWorkset);
+        }
+
+        internal bool DoesUserWorksetExist(string worksetName)
+        {
+            foreach(Workset workset in _userWorksetCollector)
+            {
+                if(workset.Name == worksetName)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
