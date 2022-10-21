bdca9de49161a5e644b38b8fd128eb39553bbd3c
AleksandrMaslov <amaslov8910@gmail.com>
2022-10-22 01:38:10 +0300

    Add RevitLinkElement class

---
+++ RevitSpacesManager/Models/RevitLinkElement.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.Revit.DB;
+
+namespace RevitSpacesManager.Models
+{
+    internal class RevitLinkElement
+    {
+        internal RevitLinkInstance RevitLinkInstance { get; set; }
+        internal Document Document { get; set; }
+
+        internal RevitLinkElement(RevitLinkInstance revitLinkInstance)
+        {
+            RevitLinkInstance = revitLinkInstance;
+            Document = RevitLinkInstance.GetLinkDocument();
+        }
+    }
+}
