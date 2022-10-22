e6379685102688eede44184706e776d77249dbc6
AleksandrMaslov <amaslov8910@gmail.com>
2022-10-22 22:46:03 +0300

    Add SpaceElement class

---
+++ RevitSpacesManager/Models/SpaceElement.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+
+namespace RevitSpacesManager.Models
+{
+    internal class SpaceElement
+    {
+        internal Space Space { get; set; }
+        internal int Id { get; set; }
+        internal string Name { get; set; }
+        internal string PhaseName { get; set; }
+
+
+        internal SpaceElement(Space space)
+        {
+            Space = space;
+            Id = space.Id.IntegerValue;
+            Name = space.Name;
+            PhaseName = space.get_Parameter(BuiltInParameter.ROOM_PHASE).AsValueString();
+        }
+    }
+}
