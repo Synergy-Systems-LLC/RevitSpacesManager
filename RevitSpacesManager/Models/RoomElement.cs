ec1ad7681ad48f61382ce3972d9e6aa06e41108a
AleksandrMaslov <amaslov8910@gmail.com>
2022-10-22 22:52:52 +0300

    Add RoomElement class

---
+++ RevitSpacesManager/Models/RoomElement.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+
+namespace RevitSpacesManager.Models
+{
+    internal class RoomElement
+    {
+        internal Room Room { get; set; }
+        internal int Id { get; set; }
+        internal string Name { get; set; }
+        internal string PhaseName { get; set; }
+
+
+        internal RoomElement(Room room)
+        {
+            Room = room;
+            Id = room.Id.IntegerValue;
+            Name = room.Name;
+            PhaseName = room.get_Parameter(BuiltInParameter.ROOM_PHASE).AsValueString();
+        }
+    }
+}
