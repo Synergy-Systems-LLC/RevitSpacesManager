514ce7c60b72dea85f9b3fdf8f103ffccc7bad69
AleksandrMaslov <amaslov8910@gmail.com>
2022-10-23 21:39:42 +0300

    Add combobox properties to VM

--- RevitSpacesManager/ViewModels/MainWindowViewModel.cs
+++ RevitSpacesManager/ViewModels/MainWindowViewModel.cs
@@ -6,31 +6,68 @@ namespace RevitSpacesManager.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
-        #region Phases Property
-        private List<PhaseElement> _phases;
-        public List<PhaseElement> Phases
+        #region CurrentDocumentPhases Property
+        private List<PhaseElement> _currentDocumentPhases;
+        public List<PhaseElement> CurrentDocumentPhases
         {
-            get => _phases;
-            set => Set(ref _phases, value);
+            get => _currentDocumentPhases;
+            set => Set(ref _currentDocumentPhases, value);
         }
         #endregion
 
-        #region LinkedRevitDocuments Property
-        private List<RevitDocument> _linkedRevitDocuments;
-        public List<RevitDocument> LinkedRevitDocuments
+        #region CurrentDocumentPhaseSelected Property
+        private PhaseElement _currentDocumentPhaseSelected;
+        public PhaseElement CurrentDocumentPhaseSelected
         {
-            get => _linkedRevitDocuments;
-            set => Set(ref _linkedRevitDocuments, value);
+            get => _currentDocumentPhaseSelected;
+            set => Set(ref _currentDocumentPhaseSelected, value);
         }
         #endregion
+
+        #region LinkedDocuments Property
+        private List<RevitDocument> _linkedDocuments;
+        public List<RevitDocument> LinkedDocuments
+        {
+            get => _linkedDocuments;
+            set => Set(ref _linkedDocuments, value);
+        }
+        #endregion
+
+        #region LinkedDocumentSelected Property
+        private RevitDocument _linkedDocumentSelected;
+        public RevitDocument LinkedDocumentSelected
+        {
+            get => _linkedDocumentSelected;
+            set => Set(ref _linkedDocumentSelected, value);
+        }
+        #endregion
+
+        #region LinkedDocumentPhases Property
+        private List<PhaseElement> _linkedDocumentPhases;
+        public List<PhaseElement> LinkedDocumentPhases
+        {
+            get => _linkedDocumentPhases;
+            set => Set(ref _linkedDocumentPhases, value);
+        }
+        #endregion
+
+        #region LinkedDocumentPhaseSelected Property
+        private PhaseElement _linkedDocumentPhaseSelected;
+        public PhaseElement LinkedDocumentPhaseSelected
+        {
+            get => _linkedDocumentPhaseSelected;
+            set => Set(ref _linkedDocumentPhaseSelected, value);
+        }
+        #endregion
+
 
         private readonly MainModel _mainModel;
 
         public MainWindowViewModel()
         {
             _mainModel = new MainModel();
-            Phases = _mainModel.CurrentRevitDocument.Phases;
-            LinkedRevitDocuments = _mainModel.LinkedRevitDocuments;
+            CurrentDocumentPhases = _mainModel.CurrentRevitDocument.Phases;
+            LinkedDocuments = _mainModel.LinkedRevitDocuments;
         }
     }
 }
