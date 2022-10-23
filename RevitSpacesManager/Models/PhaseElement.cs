d082760d3707a2cd216df94c7903d64135334efb
AleksandrMaslov <amaslov8910@gmail.com>
2022-10-23 16:35:38 +0300

    Add PhaseElement class

---
+++ RevitSpacesManager/Models/PhaseElement.cs
@@ -0,0 +1,18 @@
+﻿using Autodesk.Revit.DB;
+
+namespace RevitSpacesManager.Models
+{
+    internal class PhaseElement
+    {
+        Phase Phase { get; set; }
+        internal int Id { get; set; }
+        internal string Name { get; set; }
+
+        internal PhaseElement(Phase phase)
+        {
+            Phase = phase;
+            Id = Phase.Id.IntegerValue;
+            Name = Phase.Name;
+        }
+    }
+}
