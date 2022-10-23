ab58e4fb9804a34ab499c60bb84f8758a8a48ef0
AleksandrMaslov <amaslov8910@gmail.com>
2022-10-23 17:05:43 +0300

    Add SyncSpaces and Rooms methods to PhaseElement class

--- RevitSpacesManager/Models/PhaseElement.cs
+++ RevitSpacesManager/Models/PhaseElement.cs
@@ -8,14 +8,24 @@ namespace RevitSpacesManager.Models
         internal Phase Phase { get; set; }
         internal int Id { get; set; }
         internal string Name { get; set; }
-        internal List<SpaceElement> Spaces;
-        internal List<RoomElement> Rooms;
+        internal List<SpaceElement> Spaces { get; set; }
+        internal List<RoomElement> Rooms { get; set; }
 
         internal PhaseElement(Phase phase)
         {
             Phase = phase;
             Id = Phase.Id.IntegerValue;
             Name = Phase.Name;
+        }
+
+        internal void SyncSpaces(List<SpaceElement> spaces)
+        {
+            Spaces = spaces;
+        }
+
+        internal void SyncRooms(List<RoomElement> rooms)
+        {
+            Rooms = rooms;
         }
     }
 }
