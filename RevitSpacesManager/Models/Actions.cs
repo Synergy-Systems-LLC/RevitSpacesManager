d09188bf097ae9823913cd12ed6b006ce8744f86
AleksandrMaslov <amaslov8910@gmail.com>
2022-11-03 00:06:00 +0300

    Add enumerator description

--- RevitSpacesManager/Models/Actions.cs
+++ RevitSpacesManager/Models/Actions.cs
@@ -1,5 +1,8 @@
 ﻿namespace RevitSpacesManager.Models
 {
+    /// <summary>
+    /// Определяет тип действия для создания объекта класса MessageGenerator
+    /// </summary>
     public enum Actions
     {
         Create = 0,
