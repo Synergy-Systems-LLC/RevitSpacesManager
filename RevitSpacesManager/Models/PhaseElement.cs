22d08d24f87aca7d93cd1f04b976e55e7ac824e5
AleksandrMaslov <amaslov8910@gmail.com>
2022-11-05 12:16:12 +0300

    Refactor PhaseElement class

--- RevitSpacesManager/Models/PhaseElement.cs
+++ RevitSpacesManager/Models/PhaseElement.cs
@@ -5,32 +5,18 @@ namespace RevitSpacesManager.Models
 {
     internal class PhaseElement
     {
-        public string Name { get; set; }
-        public int NumberOfSpaces { get => Spaces.Count; }
-        public int NumberOfRooms { get => Rooms.Count; }
-        public string SpacesItemName { get => $"{NumberOfSpaces} Space{PluralSuffix(NumberOfSpaces)} - {Name}"; } 
-        public string RoomsItemName { get => $"{NumberOfRooms} Room{PluralSuffix(NumberOfRooms)} - {Name}"; } 
+        public string SpacesItemName => $"{NumberOfSpaces} Space{PluralSuffix(NumberOfSpaces)} - {Name}"; 
+        public string RoomsItemName => $"{NumberOfRooms} Room{PluralSuffix(NumberOfRooms)} - {Name}";
 
-        internal Phase Phase { get; set; }
-        internal int Id { get; set; }
+        internal string Name { get; set; }
         internal List<SpaceElement> Spaces { get; set; } = new List<SpaceElement>();
         internal List<RoomElement> Rooms { get; set; } = new List<RoomElement>();
+        internal int NumberOfSpaces => Spaces.Count;
+        internal int NumberOfRooms => Rooms.Count;
 
         internal PhaseElement(Phase phase)
         {
-            Phase = phase;
-            Id = Phase.Id.IntegerValue;
-            Name = Phase.Name;
-        }
-
-        internal void SyncSpaces(List<SpaceElement> spaces)
-        {
-            Spaces = spaces;
-        }
-
-        internal void SyncRooms(List<RoomElement> rooms)
-        {
-            Rooms = rooms;
+            Name = phase.Name;
         }
 
         private string PluralSuffix(int number)
