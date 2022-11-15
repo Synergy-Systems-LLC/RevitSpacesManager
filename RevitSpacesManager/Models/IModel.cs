6e58360f39039068cf55ad7ccbc595c583966e28
AleksandrMaslov <amaslov8910@gmail.com>
2022-11-15 20:57:41 +0300

    Add IModel interface

---
+++ RevitSpacesManager/Models/IModel.cs
@@ -0,0 +1,15 @@
+﻿namespace RevitSpacesManager.Models
+{
+    internal interface IModel
+    {
+        RevitDocument RevitDocument { get; set; }
+
+        void DeleteAll();
+
+        void DeleteSelected(); 
+
+        void CreateAll();
+
+        void CreateSelected();
+    }
+}
