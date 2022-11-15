0a6a34692258729ed208d386ceb2e90668ebd932
AleksandrMaslov <amaslov8910@gmail.com>
2022-11-15 21:08:52 +0300

    Add SpacesModel and RoomsModel classes

---
+++ RevitSpacesManager/Models/RoomsModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RevitSpacesManager.Models
+{
+    internal class SpacesModel : IModel
+    {
+        public RevitDocument RevitDocument { get; set; }
+
+        internal SpacesModel(RevitDocument revitDocument)
+        {
+            RevitDocument = revitDocument;
+        }
+
+        public void CreateAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateSelected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteSelected()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RevitSpacesManager/Models/SpacesModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RevitSpacesManager.Models
+{
+    internal class RoomsModel : IModel
+    {
+        public RevitDocument RevitDocument { get; set; }
+
+        internal RoomsModel(RevitDocument revitDocument)
+        {
+            RevitDocument = revitDocument;
+        }
+
+        public void CreateAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateSelected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteSelected()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
